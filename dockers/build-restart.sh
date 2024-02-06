9b0f2c573aadac765e137b5a97f09c4a62d0996c
barbadosian <jiajunm@uchicago.edu>
2024-02-06 13:49:39 -0600

    remove persistent volume from docker-compose

--- dockers/build-restart.sh
+++ dockers/build-restart.sh
@@ -1,5 +1,5 @@
 # Build docker
-docker build -t hdfs-runtime:latest -f Dockerfile ..
+docker build --no-cache -t hdfs-runtime:latest -f Dockerfile ..
 
 # Restart docker-compose
 docker-compose stop && docker-compose rm -f
